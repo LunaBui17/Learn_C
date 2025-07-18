8ea4db7212624cbbab40111988a1128734ed8979
LunaBui17 <luan2k.duc@gmail.com>
2025-07-18 23:37:32 +0700

    18/7/2022: pointer, array, data type.

--- 1.c
+++ 1.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
-#include <conio.h>
 
 int main()
 {
-    void *ptr = NULL;
+    // khai bao con tro void
+    //  * là khai báo con trỏ
+    // void *ptr = NULL;
+    // khai bao bien int a = 11
     int a = 11;
-    ptr = &a;
+    // gán giá trị con trỏ bằng địa chỉ biến a
+    // & là lấy địa chỉ của biến
+    // ptr = &a;
+    void *ptr = &a;
+
     printf("kich thuoc cua con tro: %d\n", sizeof(ptr));
+    // %p là in địa chỉ và (giá trị con trỏ)
+    // * là lấy giá trị con trỏ
     printf("dia chi con tro : %p", *((int *)ptr));
     return 0;
 }
-
-fasdfasdgghgasg
