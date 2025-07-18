63a0d85252e3e73f564e2e5503d2a6301ef4e083
LunaBui17 <luan2k.duc@gmail.com>
2025-07-18 22:54:42 +0700

    test

--- 1.c
+++ 1.c
@@ -3,10 +3,12 @@
 
 int main()
 {
-    void *ptr =NULL;
+    void *ptr = NULL;
     int a = 11;
     ptr = &a;
     printf("kich thuoc cua con tro: %d\n", sizeof(ptr));
-    printf("dia chi con tro : %p", *((int*)ptr));
+    printf("dia chi con tro : %p", *((int *)ptr));
     return 0;
 }
+
+fasdfasdgghgasg
